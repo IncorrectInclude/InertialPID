17748cd6002a23665c59908b8c8508be135e3ea3
IncorrectInclude <46076855+IncorrectInclude@users.noreply.github.com>
2021-01-27 22:08:31 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Andrew                                           */
+/*    Created:      Wed Jan 27 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+vex::inertial gyroAcc(vex::PORT10);
+
+
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+}
